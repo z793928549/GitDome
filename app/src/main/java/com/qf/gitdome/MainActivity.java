5b3b1e623adae571c4f9820080655ae524158371
z793928549 <793928549@qq.com>
2017-01-12 16:49:11 +0800

    第一次上传

--- app/src/main/java/com/qf/gitdome/MainActivity.java
+++ app/src/main/java/com/qf/gitdome/MainActivity.java
@@ -1,5 +1,8 @@
 package com.qf.gitdome;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static class Adapter extends FragmentStatePagerAdapter{
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
     }
 }
